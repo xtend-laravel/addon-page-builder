016748c5505ecc503a9bf2518fc23c146747b988
Adam Lee <adam@codelabx.ltd>
2023-04-10 01:56:06 +0100

    Handle data model replacements WIP

--- src/Restify/Presenters/WidgetPresenter.php
+++ src/Restify/Presenters/WidgetPresenter.php
@@ -2,19 +2,52 @@
 
 namespace XtendLunar\Addons\PageBuilder\Restify\Presenters;
 
+use Illuminate\Database\Eloquent\Model;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use XtendLunar\Addons\RestifyApi\Restify\Presenters\PresenterResource;
 
 class WidgetPresenter extends PresenterResource implements Presentable
 {
+    protected ?string $modelClassForReplacements;
+
     public function transform(RestifyRequest $request): array
     {
+        if ($this->data['data'] ?? false) {
+            $this->modelClassForReplacements = $this->data['data']['model'] ?? null;
+            $this->data['data'] = $this->handleDataReplacementsRecursive($this->data['data']);
+        }
+
         if ($this->data['type'] === 'Collection') {
             $this->data['items'] = $this->getter($request, 'items-collection');
         }
 
         return $this->data;
+    }
+
+    protected function handleDataReplacementsRecursive(array $data): array
+    {
+        return collect($data)->map(
+            fn ($item) => is_array($item) ? $this->prepareData(
+                item: $this->handleDataReplacementsRecursive($item),
+            ) : $item)->toArray();
+    }
+
+    protected function prepareData(array $item): array
+    {
+        return collect($item)->every(
+            fn($value) => is_string($value),
+        ) ? $this->handleReplacement($item) : $item;
+    }
+
+    protected function handleReplacement(array $item): array
+    {
+        $identifier = $this->queryParams[$this->data['data']['identifier']] ?? null;
+        //dd($this->data['data']['lookup_field'], $lookupField, $this->queryParams, $item, $this->modelClassForReplacements);
+        //$model = resolve($this->modelClassForReplacements)->find($identifier);
+        //dd($model);
+        // We need to guess query the model and get the value
+        return [];
     }
 }
 
