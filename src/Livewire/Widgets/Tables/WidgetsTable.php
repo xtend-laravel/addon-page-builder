f1db8e39dfc5ebf8f82b595e3a272694187987c8
bangnokia <bangnokia@gmail.com>
2023-04-19 22:31:23 +0700

    change to filament table

--- src/Livewire/Widgets/Tables/WidgetsTable.php
+++ src/Livewire/Widgets/Tables/WidgetsTable.php
@@ -2,28 +2,36 @@
 
 namespace XtendLunar\Addons\PageBuilder\Livewire\Widgets\Tables;
 
-use Lunar\LivewireTables\Components\Columns\TextColumn;
-use Lunar\LivewireTables\Components\Table;
-use Lunar\LivewireTables\Support\TableBuilder;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Livewire\Component;
 use Xtend\Extensions\Lunar\Core\Models\Widget;
 
-/**
- * @property TableBuilder $tableBuilder
- */
-class WidgetsTable extends Table
+class WidgetsTable extends Component implements HasTable
 {
-    public function build()
+    use InteractsWithTable;
+
+    public function getTableQuery(): Builder|Relation
     {
-        $this->tableBuilder->addColumns([
+        return Widget::query();
+    }
+
+    public function getTableColumns(): array
+    {
+        return [
             TextColumn::make('id'),
             TextColumn::make('name'),
             TextColumn::make('type'),
             TextColumn::make('component'),
-        ]);
+        ];
     }
 
-    public function getData()
+    public function render()
     {
-        return Widget::paginate();
+        return view('adminhub::livewire.components.tables.base-table')
+            ->layout('adminhub::layouts.base');
     }
 }
