c81306a2f7d05a0e767a0a7178dd36a1490a7f09
Adam Lee <adam@codelabx.ltd>
2023-04-24 00:27:09 +0100

    Settings collection 4 columns and all collections

--- src/Components/CollectionWidget.php
+++ src/Components/CollectionWidget.php
@@ -15,16 +15,15 @@ abstract class CollectionWidget implements Widget
     {
         return [
             Fieldset::make('Settings collection')
+                ->columns(4)
                 ->schema([
                     Toggle::make('params.gallery.enable')->inline(false)->columnSpanFull(),
-                    TextInput::make('params.gallery.layout'),
+                    //TextInput::make('params.gallery.layout'),
                     Select::make('params.sort')->options(['created_at' => 'Created at', 'updated_at' => 'Updated at']),
                     Select::make('params.order')->options(['asc' => 'Ascending', 'desc' => 'Descending']),
-                    TextInput::make('params.limit'),
+                    TextInput::make('params.limit')->columnSpan(1),
                     Select::make('params.collection_id')
-                        ->options(Collection::where('type', 'category')
-                            ->get()
-                            ->mapWithKeys(fn($collection) => [$collection->id => $collection->translateAttribute('name')]))
+                        ->options(Collection::all()->mapWithKeys(fn($collection) => [$collection->id => $collection->translateAttribute('name')]))
                         ->label('Collection')
                         ->searchable(),
                 ]),
