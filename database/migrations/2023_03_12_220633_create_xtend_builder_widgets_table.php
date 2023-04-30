5788201ec21bd4aff47e918cff3eba9e02f05e71
Adam Lee <adam@codelabx.ltd>
2023-04-30 16:34:07 +0100

    Move migrations and seeder to builder

---
+++ database/migrations/2023_03_12_220633_create_xtend_builder_widgets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use XtendLunar\Addons\PageBuilder\Enums\WidgetType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('xtend_builder_widgets', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', array_column(WidgetType::cases(), 'name'))->nullable();
+            $table->string('name');
+            $table->string('component');
+            $table->integer('col_start')->nullable();
+            $table->integer('row_start')->nullable();
+            $table->integer('cols');
+            $table->integer('rows');
+            $table->json('data')->nullable();
+            $table->json('params')->nullable();
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('xtend_builder_widgets');
+    }
+};
---
+++ database/migrations/2023_03_12_220635_create_xtend_builder_widget_slot_item_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('xtend_builder_widget_slot_item', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('widget_id');
+            $table->foreignId('widget_slot_id');
+            $table->integer('slot_col_start')->nullable();
+            $table->integer('slot_row_start')->nullable();
+            $table->integer('slot_cols')->nullable();
+            $table->integer('slot_rows')->nullable();
+            $table->integer('position')->default(1)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('xtend_builder_widget_slot_item');
+    }
+};
