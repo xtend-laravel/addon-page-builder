dbb578120caca441dbe98a6cf6c8c0f88114bb39
Adam Lee <adam@codelabx.ltd>
2023-05-17 01:04:14 +0100

    New products auto

--- src/Components/CollectionWidget.php
+++ src/Components/CollectionWidget.php
@@ -2,6 +2,7 @@
 
 namespace XtendLunar\Addons\PageBuilder\Components;
 
+use Filament\Forms\Components\Card;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -17,18 +18,26 @@ abstract class CollectionWidget implements Widget
             Fieldset::make('Settings collection')
                 ->columns(4)
                 ->schema([
-                    Toggle::make('params.gallery.enable')->inline(false)->columnSpanFull(),
+                    Toggle::make('params.collection.newest_auto')
+                        ->label('Newest Auto Collection')
+                        ->inline(false)
+                        ->reactive()
+                        ->columnSpanFull(),
                     //TextInput::make('params.gallery.layout'),
-                    Select::make('params.sort')->options(['created_at' => 'Created at', 'updated_at' => 'Updated at']),
-                    Select::make('params.order')->options(['asc' => 'Ascending', 'desc' => 'Descending']),
-                    TextInput::make('params.limit')->columnSpan(1),
+                    Card::make()->schema([
+                        Select::make('params.sort')->options(['created_at' => 'Created at', 'updated_at' => 'Updated at'])->columnSpan(1),
+                        Select::make('params.order')->options(['asc' => 'Ascending', 'desc' => 'Descending'])->columnSpan(1),
+                    ])->columnSpanFull(),
+                    TextInput::make('params.limit')->columnSpan(2),
                     Select::make('params.collection_id')
+                        ->hidden(fn(\Closure $get) => $get('params.collection.newest_auto'))
                         ->options(Collection::all()->mapWithKeys(
-                            fn($collection) => [$collection->id => $collection->translateAttribute('name').' ('.($collection->parent?->translateAttribute('name') ?? 'Root').')'])
+                            fn($collection) => [$collection->id => $collection->translateAttribute('name')])
                         )
                         ->label('Collection')
                         ->multiple()
-                        ->searchable(),
+                        ->searchable()
+                        ->columnSpan(2),
                 ]),
         ];
     }
