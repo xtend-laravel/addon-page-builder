508eebccb8c4df8bc74a0d07edad4f2b1b65e005
Adam Lee <adam@codelabx.ltd>
2023-08-16 23:55:28 +0100

    Allow to query page by widget slot identifier

--- src/Restify/CmsPageRepository.php
+++ src/Restify/CmsPageRepository.php
@@ -2,7 +2,12 @@
 
 namespace XtendLunar\Addons\PageBuilder\Restify;
 
+use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Str;
 use XtendLunar\Addons\PageBuilder\Models\CmsPage;
+use XtendLunar\Addons\PageBuilder\Models\WidgetSlot;
 use XtendLunar\Addons\PageBuilder\Restify\Presenters\CmsPagePresenter;
 use XtendLunar\Addons\RestifyApi\Restify\Repository;
 
@@ -13,4 +18,14 @@ class CmsPageRepository extends Repository
     public static string $presenter = CmsPagePresenter::class;
 
     public static bool|array $public = true;
+
+    protected static function booting(): void
+    {
+        $identifier = Str::slug(request()->route()->parameter('repositoryId'), '_');
+        $widgetSlot = WidgetSlot::query()->firstWhere('identifier', $identifier);
+
+        if ($widgetSlot) {
+            request()->route()->setParameter('repositoryId', $widgetSlot->page->id);
+        }
+    }
 }
