c964a23a9d25a2ba2308e7c99d9555216aca1e2a
Adam Lee <adam@codelabx.ltd>
2024-02-02 23:19:47 +0000

    Update blog presenter

--- src/Restify/Presenters/BlogPostPresenter.php
+++ src/Restify/Presenters/BlogPostPresenter.php
@@ -2,6 +2,7 @@
 
 namespace XtendLunar\Addons\PageBuilder\Restify\Presenters;
 
+use Illuminate\Support\Facades\Storage;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use XtendLunar\Addons\RestifyApi\Restify\Presenters\PresenterResource;
@@ -10,7 +11,16 @@ class BlogPostPresenter extends PresenterResource implements Presentable
 {
     public function transform(RestifyRequest $request): array
     {
-        return $this->data;
+        return [
+            'id' => $this->data['id'],
+            'title' => $this->repository->model()->translate('title'),
+            'slug' => $this->data['slug'],
+            'excerpt' => $this->repository->model()->translate('excerpt'),
+            'content' => $this->repository->model()->translate('content'),
+            'banner' => Storage::disk('do')->url($this->data['banner']),
+            'created_at' => $this->data['created_at'],
+            'updated_at' => $this->data['updated_at'],
+        ];
     }
 }
 
