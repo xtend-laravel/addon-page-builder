020132ac6e04c4c968bdb1be0c827f44bbc3bce6
Adam Lee <adam@codelabx.ltd>
2024-02-05 23:12:36 +0000

    Fix issues with blog presenter

--- src/Restify/Presenters/BlogPostPresenter.php
+++ src/Restify/Presenters/BlogPostPresenter.php
@@ -2,6 +2,7 @@
 
 namespace XtendLunar\Addons\PageBuilder\Restify\Presenters;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
 use XtendLunar\Addons\RestifyApi\Restify\Contracts\Presentable;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
@@ -11,15 +12,21 @@ class BlogPostPresenter extends PresenterResource implements Presentable
 {
     public function transform(RestifyRequest $request): array
     {
+        $hasBanner = $this->data['banner'] && Storage::disk('do')->exists($this->data['banner']);
         return [
             'id' => $this->data['id'],
             'title' => $this->repository->model()->translate('title'),
             'slug' => $this->data['slug'],
             'excerpt' => $this->repository->model()->translate('excerpt'),
             'content' => $this->repository->model()->translate('content'),
-            'banner' => Storage::disk('do')->url($this->data['banner']),
-            'created_at' => $this->data['created_at'],
-            'updated_at' => $this->data['updated_at'],
+            'banner' => $hasBanner ? Storage::disk('do')->url($this->data['banner']) : null,
+            'category' => [
+                'id' => $this->repository->model()->category->id,
+                'name' => $this->repository->model()->category->translate('name'),
+                'slug' => $this->repository->model()->category->slug,
+            ],
+            'created_at' => Carbon::parse($this->data['created_at'])->format('M d, Y H:i a'),
+            'updated_at' => Carbon::parse($this->data['updated_at'])->format('M d, Y H:i a'),
         ];
     }
 }
